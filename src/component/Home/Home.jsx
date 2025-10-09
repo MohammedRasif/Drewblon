0e77505dcfdd1050e10346da4d604f3cc068acaa
MohammedRasif <mohammed660012@gmail.com>
2025-10-09 16:23:45 +0600

    add new funcation

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Pages/banner";
+import Improve from "../Pages/Improve";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Improve/>
         </div>
     );
 }
